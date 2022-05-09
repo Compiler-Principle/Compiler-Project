7297150725fe39906e4a1994b2efb797a4af4180
MapleQwQ <921325219@qq.com>
2022-05-09 17:46:51 +0800

    iniit AST

---
+++ AST.h
@@ -0,0 +1,18 @@
+#ifndef AST_H
+#define AST_H
+#include <vector>
+#include <string>
+#include <map>
+
+
+class baseAST{
+public:
+    int id;    //唯一id标识
+    Type type; //节点类型
+    DataType dataType;   //节点数据类型
+
+
+};
+
+
+#endif
