5bb963016a5cb3beab6955cf9f88834b8756d86b
MapleQwQ <921325219@qq.com>
2022-05-09 18:05:47 +0800

    AST.h 新增

--- AST.h
+++ AST.h
@@ -3,16 +3,36 @@
 #include <vector>
 #include <string>
 #include <map>
+typedef enum Type
+{
+    operator,
+    var,
+} Type;
 
+typedef enum DataType
+{
+    
+    nonedt,
+    void,
+    integer,
+    float,
+    string,
+    function,
+} DataType;
 
 class baseAST{
 public:
     int id;    //唯一id标识
     Type type; //节点类型
     DataType dataType;   //节点数据类型
-
-
+    int child_num;
+    std::vector<AST *> *children;
+    
+    AST(Type type, const char *name);
+    ~AST();
+    void Insert(AST *);
+    void print(void);
 };
-
+class 
 
 #endif
