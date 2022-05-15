159aba83ecb3efb106c23ecf89011db0b784887a
L1ght <2504082957@qq.com>
2022-05-15 15:23:24 +0800

    change the output color of info

--- info/info.cpp
+++ info/info.cpp
@@ -3,12 +3,15 @@
 
 void info(InfoLevel level, std::string message){
     if(level == InfoLevel::INFO){
-        std::cout << "[INFO]: " << message << std::endl;
+        std::cout << "\033[32m[ INFO ]: \033[0m" ;
+        std::cout << message << std::endl;
     }
     else if(level == InfoLevel::WARNING){
-        std::cout << "[WARNING]: " << message << std::endl;
+        std::cout << "\033[33m[ WARNING ]: \033[0m" ;
+        std::cout << message << std::endl;
     }
     else if(level == InfoLevel::ERROR){
-        std::cerr << "[ERROR]: " << message << std::endl;
+        std::cout << "\033[31m[ ERROR ]: \033[0m" ;
+        std::cerr << message << std::endl;
     }
 }
