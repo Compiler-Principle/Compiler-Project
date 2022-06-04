7cabc21561327f0887660ed08c59aa8d6e0d12e3
FanBB2333 <FanBB2333@users.noreply.github.com>
2022-06-04 17:03:44 +0000

    change build path

---
+++ component---src-pages-404-js-ea3a5b498905eef1210b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,n){n.r(e);var u=n(7294),a=n(5592),l=n(262);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ea3a5b498905eef1210b.js.map
