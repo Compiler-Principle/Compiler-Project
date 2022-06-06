f4a804f3b658d7d9beddf3754db02d161e68d12f
FanBB2333 <FanBB2333@users.noreply.github.com>
2022-06-06 08:46:53 +0000

    test page-2 jsx

--- component---src-pages-page-2-js-296db8cd5d5bada5cc9c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(5592),c=a(262);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-296db8cd5d5bada5cc9c.js.map
